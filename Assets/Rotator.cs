7fc6995a53945f06a5804525f9526e4a81ffa8f0
thisfn <thisfenu@gmail.com>
2017-11-23 16:47:04 -0300

    BONUS: Progressão

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -3,9 +3,11 @@
 public class Rotator : MonoBehaviour
 {
 	public float value;
+	public float increase;
 
 	private void Update()
 	{
+		value = value + (Time.deltaTime * increase);
 		transform.Rotate(0, 0, value * Time.deltaTime);
 	}
 }
