0c869cceb21a522311c66adae4ae6d8ccfc7a4e5
thisfn <thisfenu@gmail.com>
2017-11-23 12:58:40 -0300

    Random.Range - 1

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,12 @@ public class Player : MonoBehaviour
 	public Rigidbody2D rb;
 	public float force;
 
+	private void Start()
+	{
+		int num = Random.Range(0, 4);
+		print(num);
+	}
+
 	private void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.Mouse0))
